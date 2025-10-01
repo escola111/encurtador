d63c5ffec375d5a88f677289153cc237526c5e32
escola111 <githubhtml.css@gmail.com>
2025-10-01 10:06:39 -0300

    add possivel final

---
+++ css/encurtador/css/encurtador/html/encurtador/html/java/butao.js
@@ -0,0 +1,25 @@
+const form = document.getElementById("form");
+const linkEncurtado = document.getElementById("linkEncurtado");
+const botaoCopiar = document.getElementById("botaoCopiar");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const inputUrl = this.querySelector('input[type="url"]').value;
+
+  const urlEncurtada = "https://encurtador.com/" + btoa(inputUrl).slice(0, 8);
+
+  linkEncurtado.textContent = urlEncurtada;
+  botaoCopiar.style.display = "inline-block";
+});
+
+botaoCopiar.addEventListener("click", function () {
+  const textoParaCopiar = linkEncurtado.textContent;
+  navigator.clipboard
+    .writeText(textoParaCopiar)
+    .then(() => {
+      alert("Link copiado para a área de transferência!");
+    })
+    .catch(() => {
+      alert("Não foi possível copiar o link.");
+    });
+});
